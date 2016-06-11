57825497f4a12605fa6e65b0a0ec0039d811b49d
user <user@192.168.1.100>
2016-06-11 16:33:36 +0530

    initial check in

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String []arg)
+	{
+		SalesData data = new SalesData();
+		
+		displaygreeting();
+		data.display();
+	}
+	public static void displaygreeting()
+	{
+		System.out.println("sdfasdfasdfasdf");
+		System.out.println("1234567890");
+	}
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package ca.demo.salestool;
+
+public class SalesData {
+
+	public void display()
+	{
+		System.out.println("sales data display");
+	}
+}
